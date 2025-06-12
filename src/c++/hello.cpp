756c385f89f35390d8d0dc5d8dc69157b5b778ea
Paulo Sergio Soler Filho <paulosolerdev@gmail.com>
2025-06-12 00:33:21 -0300

    começando aprender c++ na prática

---
+++ src/c++/hello.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     cout << "Olá, Mundo!" << endl;
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Olá, Mundo!" << endl;
+    cout << "Olá, Mais uma vez" << endl;
+    return 0;
+}
---
+++ src/c++/var.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+    char letra;
+    double num_real;
+    bool teste;
+    cout << "Este programa criou 4 variáveis!" << endl;
+    return 0;
+}
