d066d408e3e42b2cf4b30cdc5453eaaa53e80924
Paulo Soler <paulosolerdev@gmail.com>
2025-06-25 09:19:24 -0300

    use a cabeça

--- src/python/clevison/exercicios/Desafios_4/teste.py
+++ src/python/clevison/exercicios/Desafios_4/teste.py
@@ -1,1 +1,6 @@
-print("Hello world python.")
+def soma_digitos(n):
+    return sum(int(digit) for digit in str(n))
+
+print(soma_digitos(123))
+print(soma_digitos(547))
+print(soma_digitos(656))
--- src/python/use_a_cabeca/panic.py
+++ src/python/use_a_cabeca/panic.py
@@ -1,24 +1,40 @@
+# phrase = "Don't panic!"
+# plist = list(phrase)
+# print(phrase)
+# print(plist)
+
+# for i in range(4):
+#     plist.pop()
+# print(plist)
+
+# plist.pop(0)
+# print(plist)
+
+# plist.remove("'")
+# print(plist)
+
+# plist.extend([plist.pop(), plist.pop()])
+# print(plist)
+
+# plist.insert(2, plist.pop(3))
+# print(plist)
+
+# new_phrase = ''.join(plist)
+# print(new_phrase)
+
+#=======================================================#
+
 phrase = "Don't panic!"
 plist = list(phrase)
 print(phrase)
 print(plist)
 
-for i in range(4):
-    plist.pop()
-print(plist)
+new_phrase = ''.join(plist[1:3])
 
-plist.pop(0)
+new_phrase = ''.join([plist[1], plist[2], plist[5], plist[4], plist[3]])
 print(plist)
+print(new_phrase)
 
-plist.remove("'")
-print(plist)
-
-plist.extend([plist.pop(), plist.pop()])
-print(plist)
-
-plist.insert(2, plist.pop(3))
-print(plist)
-
-new_phrase = ''.join(plist)
+new_phrase = ''.join([plist[1], plist[2], plist[5], plist[7], plist[6], plist[4]])
 print(plist)
 print(new_phrase)
