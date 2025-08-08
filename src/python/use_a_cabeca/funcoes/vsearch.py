366335fef35242d2efa2a34e76879252f2a0dba9
Paulo Soler <paulosolerdev@gmail.com>
2025-08-08 14:02:53 -0300

    funções

--- src/python/use_a_cabeca/funcoes/vsearch.py
+++ src/python/use_a_cabeca/funcoes/vsearch.py
@@ -1,9 +1,23 @@
-def search4vowels():
-    """Display any vowels found in an asked for word."""
-    vowels = set('aeiou')
-    word = input("Enter a word: ")
-    found = vowels.intersection(set(word))
-    for vowel in found:
-        print(f"{vowel}")
+# def search4vowels(word):
+#     """Display any vowels found in a supplied word."""
+#     vowels = set('aeiou')
+#     found = vowels.intersection(set(word))
+#     for vowel in found:
+#         print(f"{vowel}")
 
-search4vowels()
+# def search4vowels(word):
+#     """Display any vowels found in a supplied word."""
+#     vowels = set('aeiou')
+#     found = vowels.intersection(set(word))
+
+#     return bool(found)
+
+def search4vowels(word):
+    """Display any vowels found in a supplied word."""
+    vowels = set('aeiou')
+    found = vowels.intersection(set(word))
+    return found
+
+print(search4vowels('Soler'))
+print(search4vowels('Sky'))
+print(search4vowels('Why'))
