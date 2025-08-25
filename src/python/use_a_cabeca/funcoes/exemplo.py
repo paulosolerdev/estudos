cd82eb7ef0d539bc8742de4210972850e83f82bd
Paulo Soler <paulosolerdev@gmail.com>
2025-08-25 08:05:22 -0300

    continuando módulos

---
+++ src/python/use_a_cabeca/funcoes/exemplo.py
@@ -0,0 +1,25 @@
+# def mudar_numero(n):
+#     n = n + 1
+#     print("Dentro:", n)
+
+# x = 10
+# mudar_numero(x)
+# print("Fora:", x)
+
+
+# def mudar_lista(lst):
+#     lst.append(99)
+#     print("Dentro:", lst)
+
+# x = [1, 2, 3]
+# mudar_lista(x)
+# print("Fora:", x)
+
+
+def redefinir_lista(lst):
+    lst = [0, 0, 0]   # cria nova lista
+    print("Dentro:", lst)
+
+x = [1, 2, 3]
+redefinir_lista(x)
+print("Fora:", x)
---
+++ src/python/use_a_cabeca/funcoes/tom_sarah.py
@@ -0,0 +1,19 @@
+def dobrar_lista(arg):
+    # cria uma nova lista e faz arg apontar para ela
+    arg = arg * 2
+    print("Dentro da função:", arg)
+
+numbers = [1, 2, 3]
+dobrar_lista(numbers)
+print("Fora da função:", numbers)
+
+print("---")
+
+def adicionar_numero(arg):
+    # modifica a lista existente
+    arg.append(4)
+    print("Dentro da função:", arg)
+
+numbers = [1, 2, 3]
+adicionar_numero(numbers)
+print("Fora da função:", numbers)
