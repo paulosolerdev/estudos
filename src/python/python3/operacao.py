19af8b9f0ae1090119e9440c6b2e4a0c219bc3b2
Paulo Soler <paulosolerdev@gmail.com>
2025-11-12 14:12:09 -0300

    operações

---
+++ src/python/python3/operacao.py
@@ -0,0 +1,14 @@
+a = 14
+b = 5
+
+print("Soma:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Divisão:", a / b)
+print("Divisão Inteira:", a // b)
+print("Módulo:", a % b)
+print("Exponenciação:", a ** b)
+print("Raiz Quadrada de a:", a ** 0.5)
+print("Raiz Quadrada de b:", b ** 0.5)
+print("Unário -a:", -a)
+print("Unário +b:", +b)
