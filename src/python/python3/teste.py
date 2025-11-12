efbe6a93eae75424529debda72ae8a93473eb012
Paulo Soler <paulosolerdev@gmail.com>
2025-11-12 14:36:24 -0300

    testes

---
+++ src/python/python3/teste.py
@@ -0,0 +1,19 @@
+from math import sqrt
+
+x = 9
+
+r = sqrt(x)
+
+print(f"The square root of {x} is {r}")
+
+print(f"O valor absoluto de x é {abs(-x)}")
+
+x = -9
+
+print(f"O valor absoluto de x é {abs(x)}")
+
+print(f"O valor exponencial de 2 elevado a 3 é {pow(2, 3)}")
+
+print("Valor de {0} e valor de {1}".format(x, r))
+
+print("Valor de {1} e valor de {0}".format(x, r))
