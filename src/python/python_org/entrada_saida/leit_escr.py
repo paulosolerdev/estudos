10cdbde7b4ac37520468ecf72cc6a345574d2a1a
Paulo Soler <paulosolerdev@gmail.com>
2025-12-31 08:21:20 -0300

    testes

---
+++ src/python/python_org/entrada_saida/leit_escr.py
@@ -0,0 +1,8 @@
+# f = open('arquivo_teste.txt', 'w', encoding='utf-8')
+
+
+with open('arquivo_teste.txt', encoding='utf-8') as f:
+    read_data = f.read()
+
+print(f.closed)
+print(read_data)
---
+++ src/python/python_org/entrada_saida/quad_e_cubos.py
@@ -0,0 +1,10 @@
+# for x in range(1, 11):
+#     print(repr(x).rjust(2), repr(x*x).rjust(3), end=' ')
+#     print(repr(x*x*x).rjust(4))
+
+
+import math
+
+# print('O valor de pi é aproximadamente %5.3f.' % math.pi)
+
+print(f'O valor de pi é aproximadamente {math.pi:.3f}.')
