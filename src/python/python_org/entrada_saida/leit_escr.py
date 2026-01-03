8afbe4efd9dd2e58ddbc40fda4a42b20f1ac0ad4
Paulo Soler <paulosolerdev@gmail.com>
2026-01-03 12:58:28 -0300

    finalizando entrada e saída de python.org

--- src/python/python_org/entrada_saida/leit_escr.py
+++ src/python/python_org/entrada_saida/leit_escr.py
@@ -1,12 +1,37 @@
-# f = open('arquivo_teste.txt', 'w', encoding='utf-8')
+# with open('arquivo_teste.txt', encoding='utf-8') as f:
+#     read_data = f.read()
+#     print(read_data)
 
 
-with open('arquivo_teste.txt', encoding='utf-8') as f:
-    read_data = f.read()
+# with open('arquivo_teste.txt', encoding='utf-8') as f:
+#     for line in f:
+#         print(line)
 
-# print(f.closed)
-# print(read_data)
-# print(read_data)
 
-for line in f:
-    print(line, end='')
+# with open('arquivo_teste.txt', encoding='utf-8') as f:
+#     read_data = f.read()
+
+# for line in read_data.splitlines():
+#     print(line)
+
+
+# with open('arquivo_teste.txt', encoding='utf-8') as f:
+#     read_data = f.read()
+#     f.seek(0)
+
+#     for line in f:
+#         print(line, end='')
+
+
+# f = open('arquivo_teste.txt', 'rb+')
+# print(f.write(b'0123456789abcdef'))
+# print(f.seek(5))
+# print(f.read(1))
+# print(f.seek(-3, 2))
+# print(f.read(1))
+# f.close()
+
+
+import json
+x = [1, 'lista', 'simples']
+print(json.dumps(x))
