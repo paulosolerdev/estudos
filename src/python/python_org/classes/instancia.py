38cece9bb6569288ac56179fcb7ea9007065a1ba
Paulo Soler <paulosolerdev@gmail.com>
2026-01-21 23:26:32 -0300

    salvando

---
+++ src/python/python_org/classes/instancia.py
@@ -0,0 +1,33 @@
+# class Cachorro:
+
+#     tipo = 'canino'
+
+#     def __init__(self, nome):
+#         self.nome = nome
+
+
+# d = Cachorro('Fido')
+# e = Cachorro('Buddy')
+# print(d.tipo)  # Saída: canino
+# print(e.tipo)  # Saída: canino
+# print(d.nome)  # Saída: Fido
+# print(e.nome)  # Saída: Buddy
+
+
+class Cachorro:
+
+    truques = []
+
+    def __init__(self, nome):
+        self.nome = nome
+        self.truques = []
+
+    def adicionar_truque(self, truque):
+        self.truques.append(truque)
+
+d = Cachorro('Fido')
+e = Cachorro('Buddy')
+d.adicionar_truque('rolar')
+print(d.truques)  # Saída: ['rolar']
+e.adicionar_truque('fingir de morto')
+print(e.truques)  # Saída: ['rolar', 'fingir de morto']
