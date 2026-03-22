6756f87b54dc7c0d19a732d62d0d15b35d6f4f69
Paulo Soler <paulosolerdev@gmail.com>
2026-03-22 16:18:21 -0300

    testes

---
+++ src/python/python3/ex_resolv/exemplo.py
@@ -0,0 +1,45 @@
+# # Formatando dados com f-strings
+
+# codigo = 27
+# pi = 3.1415926
+# print(f'Código do usuário: {codigo}')
+# print(f'15 em octal é {15:o}')
+# print(f'100 em hexadecimal é {100:x}')
+# print(f'pi com duas casas decimais é aproximadamente {pi:.2f}')
+# print(f'pi com três casas decimais é aproximadamente {pi:.3f}')
+# nome = 'Paulo'
+# print(f'Meu nome é {nome}')
+
+
+#=============================================#
+
+
+# Programa de listagem
+
+# soma = 0
+
+# for i in range(1, 101):
+#     soma = soma + i
+    
+# print(f'A soma dos números de 1 a 100 é {soma}')
+
+
+#=============================================#
+
+
+# teste1 = 123
+# def funcao_f():
+#     global teste2
+#     teste2 = 456
+#     for x in range(1, 10):
+#         z = x*2
+#     print(f'z = {z}')
+#     print(f'Valor de z, após o loop: {z}')
+#     print(f'Valor de teste1, dentro da função: {teste1}')
+#     print(f'Valor de teste2, dentro da função: {teste2}')    
+
+# funcao_f()
+
+# print(f'Valor de teste1, fora da função: {teste1}')
+# print(f'Valor de teste2, fora da função: {teste2}')
+# print(f'Valor de z, após o loop: {z}')
---
+++ src/python/python3/ex_resolv/teste.py
@@ -0,0 +1,16 @@
+# import pprint
+
+# t = [[[['preto', 'ciano'], 'branco', ['verde', 'vermelho']], [['magenta',
+#     'amarelo'], 'azul']]]
+
+# pprint.pprint(t, width=30)
+
+
+# import textwrap
+
+# doc = """O método wrap() é como fill(), exceto que ele retorna
+# uma lista de strings em vez de uma string grande com quebras
+# de linha para separar as linhas quebradas."""
+
+# print(textwrap.fill(doc, width=40))
+
