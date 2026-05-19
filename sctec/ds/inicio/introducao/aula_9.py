de48c52034a2b8a8ac4a29e5157ca2feba03ddcf
Paulo Soler <paulosolerdev@gmail.com>
2026-05-18 23:29:51 -0300

    testes

--- sctec/ds/inicio/introducao/aula_9.py
+++ sctec/ds/inicio/introducao/aula_9.py
@@ -1,11 +1,19 @@
-hora = 10
-humor = 'palavra'
+# hora = 10
+# humor = 'palavra'
 
-if humor == 'sono' and hora < 10:
-    print('café')
+# if humor == 'sono' and hora < 10:
+#     print('café')
 
-elif humor == 'sedento' or hora < 2:
-    print('limonada')
+# elif humor == 'sedento' or hora < 2:
+#     print('limonada')
 
-else:
-    print('água')
+# else:
+#     print('água')
+
+# =================================================== $
+
+planetas = ['Mercúrio', 'Vênus', 'Terra', 'Marte', 'Júpiter', 'Saturno', 
+            'Urano', 'Netuno']
+
+for planeta in planetas:
+    print(f'Planeta: {planeta}')
