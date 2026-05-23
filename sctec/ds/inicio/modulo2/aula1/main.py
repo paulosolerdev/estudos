22b177fce8d55985bed2c850b755e1d38b5e5e46
Paulo Soler <paulosolerdev@gmail.com>
2026-05-22 22:39:23 -0300

    salvatore

--- sctec/ds/inicio/modulo2/aula1/main.py
+++ sctec/ds/inicio/modulo2/aula1/main.py
@@ -1,20 +1,33 @@
-import pandas as pd
+# import pandas as pd
 
-# # Criando uma série
-# serie = pd.Series([10, 20, 30, 40, 50])
+# # # Criando uma série
+# # serie = pd.Series([10, 20, 30, 40, 50])
 
-# print(serie)
+# # print(serie)
 
-data = {
-    'Nome': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
-    'Idade': [25, 30, 35, 40, 45],
-    'Cidade': ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba', 'Porto Alegre']
-}
+# data = {
+#     'Nome': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+#     'Idade': [25, 30, 35, 40, 45],
+#     'Cidade': ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba', 'Porto Alegre']
+# }
 
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)
+
+# # print(df)
+
+# # print(df['Nome'])
+
+# print(df.loc[0,'Nome'])
+
+
+# import pandas as pd
+
+# df = pd.read_csv('sctec/ds/inicio/modulo2/aula1/alunos.csv')
 
 # print(df)
 
-# print(df['Nome'])
 
-print(df.loc[0,'Nome'])
+
+import pandas as pd
+
+df = pd.read_csv('dados')
