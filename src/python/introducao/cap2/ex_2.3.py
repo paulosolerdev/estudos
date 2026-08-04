8db7a26e6ef83b46ad26327f5ffc9c012a647359
Paulo Soler <paulosolerdev@gmail.com>
2026-08-04 08:14:12 -0300

    salve

---
+++ src/python/introducao/cap2/ex_2.3.py
@@ -0,0 +1,13 @@
+# a) Atribua o valor inteiro 3 à variável a.
+a = 3
+
+# b) Atribua 4 à variável b.
+b = 4
+
+# c) Atribua à variável c o valor da expressão a * a + b * b.
+c = a * a + b * b
+
+print(c)
+
+x = 7
+print(x)
